26ffc311f6230738b7989ccd405c4a970ec2bc8b
Mauro Gabriel dos Santos <gabriel.mauro.santos.3@gmail.com>
2024-04-11 20:41:56 -0300

    Add sidebar and logic to open and close based on state

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -3,11 +3,12 @@ import { Plus } from "lucide-react";
 import { Logo } from "@/components/logo";
 import { Button } from "@/components/ui/button";
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+import { MobileSidebar } from "./mobile-sidebar";
 
 export const NavBar = () => {
   return (
     <nav className="fixed z-50 top-0 w-full h-14 border-b shadow-sm bg-white flex items-center">
-      {/* Todo: Mobile sidebar */}
+      <MobileSidebar />
       <div className="flex items-center gap-x-4">
         <div className="hidden md:flex">
           <Logo />
