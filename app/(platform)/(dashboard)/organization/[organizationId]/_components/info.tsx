09dff6b3dace7674b021f731a19a29ef1a7c05b3
Mauro Gabriel dos Santos <gabriel.mauro.santos.3@gmail.com>
2024-04-16 19:49:31 -0300

    Refactor layout.tsx to add Toaster component, update use-action.ts to simplify error handling, delete form-delete.tsx, and update package.json to add sonner dependency

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/info.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { Skeleton } from "@/components/ui/skeleton";
+import { useOrganization } from "@clerk/nextjs";
+import { CreditCard } from "lucide-react";
+import Image from "next/image";
+
+export const Info = () => {
+  const { organization, isLoaded } = useOrganization();
+
+  if (!isLoaded) {
+    return <Info.Skeleton />;
+  }
+
+  return (
+    <div className="fkex items-center gap-x-4">
+      <div className="w-[68px] h-[60px] relative"  >
+        <Image
+          src={organization?.imageUrl!}
+          alt="Organization"
+          className="rounded-md object-cover"
+          width={68}
+          height={60}
+        />
+      </div>
+      <div className="space-y-1">
+        <p className="font-semibold text-xl">
+          {organization?.name}
+        </p>
+        <div className="flex items-center text-xs text-muted-foreground">
+          <CreditCard className="h-3 w-3 mr-1" />
+          Free
+        </div>
+      </div>
+    </div>
+  );
+}
+
+Info.Skeleton = function SkeletonInfo() {
+  return (
+    <div className="flex items-center gap-x-4">
+      <div className="w-[68] h-[60px] relative">
+        <Skeleton className="w-full h-full" />
+      </div>
+      <div className="space-y-2">
+        <Skeleton className="w-[200px] h-10" />
+        <div className="flex items-center">
+          <Skeleton className="h-4 w-4 mr-2" />
+          <Skeleton className="h-4 w-[100px]" />
+        </div>
+      </div>
+    </div>
+  );
+};
--- hooks/use-action.ts
+++ hooks/use-action.ts
@@ -31,10 +31,7 @@ export const useAction = <TInput, TOutput>(
           return setError('An unknown error occurred');
         }
 
-        if (result.fieldErrors) {
-          setFieldErrors(result.fieldErrors);
-
-        }
+        setFieldErrors(result.fieldErrors);
 
         if (result.error) {
           setError(result.error);
