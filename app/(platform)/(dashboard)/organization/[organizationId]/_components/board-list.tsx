9cdf4e8092691b9a60e621c809abf92ce383b7ce
Mauro Gabriel dos Santos <gabriel.mauro.santos.3@gmail.com>
2024-05-01 17:11:41 -0300

    Refactor BoardList component to include async/await logic and add SkeletonBoardList component

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -1,8 +1,28 @@
 import { FormPopover } from "@/components/form/form-popover";
 import { Hint } from "@/components/hint";
+import { Skeleton } from "@/components/ui/skeleton";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
 import { HelpCircle, User2 } from "lucide-react";
+import Link from "next/link";
+import { redirect } from "next/navigation";
 
-export const BoardList = () => {
+export const BoardList = async () => {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    return redirect("/select-org")
+  }
+
+  const boards = await db.board.findMany({
+    where: {
+      orgId: orgId
+    },
+    orderBy: {
+      createdAt: "desc"
+    }
+  });
+
   return (
     <div className="space-y-4">
       <div className="flex items-center font-semibold text-lg text-neutral-700">
@@ -10,6 +30,19 @@ export const BoardList = () => {
         Your Boards
       </div>
       <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+        {boards.map((board: any) => (
+          <Link
+            key={board.id}
+            href={'/board/${board.id}'}
+            style={{ backgroundImage: `url(${board.imageThumbUrl})` }}
+            className="group relative aspect-video bg-no-repeat bg-center bg-cover bg-sky-700 rounded-sm w-full h-full p-2 overflow-hidden"
+          >
+            <div className="absolute inset-0 bg-black/30 group-hover:bg-black/40 transition" />
+            <p className="relative semi-bold text-white">
+              {board.title}
+            </p>
+          </Link>
+        ))}
         <FormPopover sideOffset={10} side="right">
           <div role="button" className="aspect-video relative  h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition">
             <p className="text-sm">
@@ -24,6 +57,20 @@ export const BoardList = () => {
           </div>
         </FormPopover>
       </div>
+    </div>
+  );
+};
+
+BoardList.Skeleton = function SkeletonBoardList() {
+  return (
+    <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+      <Skeleton className="aspect-video p-2 w-full h-full" />
+      <Skeleton className="aspect-video p-2 w-full h-full" />
+      <Skeleton className="aspect-video p-2 w-full h-full" />
+      <Skeleton className="aspect-video p-2 w-full h-full" />
+      <Skeleton className="aspect-video p-2 w-full h-full" />
+      <Skeleton className="aspect-video p-2 w-full h-full" />
+      <Skeleton className="aspect-video p-2 w-full h-full" />
     </div>
   );
 };
