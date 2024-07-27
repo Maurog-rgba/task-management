77a8f72a98d5dcd0f525efb403dd5eca12bb77e7
Mauro Gabriel dos Santos <gabriel.mauro.santos.3@gmail.com>
2024-07-27 19:53:27 -0300

    feat: add actions to copy and delete lists on the board

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
@@ -7,6 +7,7 @@ import { List } from "@prisma/client";
 import { ElementRef, useRef, useState } from "react";
 import { toast } from "sonner";
 import { useEventListener } from "usehooks-ts";
+import { ListOptions } from "./list-options";
 
 interface ListHeaderProps {
   data: List;
@@ -114,6 +115,12 @@ export const ListHeader = ({
           {title}
         </div>
       )}
+
+      <ListOptions
+        onAddCard={() => { }}
+        data={data}
+      />
+
     </div>
   );
 };
